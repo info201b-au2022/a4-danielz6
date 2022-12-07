4ad39a38d333b5ed78ec1daf08bdf054d867b836
danielz6 <danielzhang606@gmail.com>
2022-12-06 19:08:07 -0800

    "resubmission

--- source/analysis.R
+++ source/analysis.R
@@ -10,7 +10,7 @@ source("../source/a4-helpers.R")
 #----------------------------------------------------------------------------#
 df <- read.csv("~/Documents/info201/data/incarceration_trends.csv")
 total_jail_pop <- get_data()
-View(total_jail_pop)
+
 ## Section 2  ---- 
 #----------------------------------------------------------------------------#
 # Your functions and variables might go here ... <todo: update comment>
@@ -37,14 +37,14 @@ get_year_jail_pop <- function() {
     summarise(total_jail_pop = sum(total_jail_pop))
     return(total_pop)   
 }
-get_year_jail_pop()
 # This function ... <todo:  update comment>
 plot_jail_pop_for_us <- function()  {
   ggplot(get_year_jail_pop(), aes(x = year, y = total_jail_pop)) +
     geom_bar(stat = "identity", position = "dodge") +
     labs(title = "The Distribution of Jail Population in the U.S. (1970 - 2018)",
       x = "Year",
-      y = "Total Jail Population")
+      y = "Total Jail Population") + 
+    labs(caption = "This plot shows the population distribution by time in the United States.")
   
 } 
 plot_jail_pop_for_us()
@@ -73,7 +73,8 @@ plot_jail_pop_by_states <- function(states) {
   return(ggplot(get_jail_pop_by_states(states), aes(x = year, y = total_jail_pop, colour = state)) +
          geom_point(size = 0.8, alpha = 0.09) +
          geom_smooth(size = 2) +
-           theme_minimal() + labs(title = "Jail Population Distribution in the U.S. By State (1970-2018)", x = "Year", y = "Total Jail Population")
+           theme_minimal() + labs(title = "Jail Population Distribution in the U.S. By State (1970-2018)", x = "Year", y = "Total Jail Population") +
+           labs(caption = "This plot shows the differences between the total jail population in each state.")
            )
 }
 plot_jail_pop_by_states(c("WA", "CA", "NY", "NV"))
@@ -111,7 +112,7 @@ df4 <- data.frame("State" = df$state,
                   "black_pop" = df$black_jail_pop,
                   "Total_pop" = df$total_pop)
 
-ratio_diff <- df4 %>% summarize(state = State, pop_ratio = Total_pop/sum(Total_pop))
+ratio_diff <- df4 %>% summarize(state = State, pop_ratio = Total_pop*100/max(Total_pop))
 
 
 
@@ -143,8 +144,8 @@ plot_state <- function() {
   scale_fill_continuous(low = "#D8D7D7", high = "#F0A8A8", limits = c(0,1)) +
     labs(fill = "Ratio",
          caption = "The population of total immates across America",
-         x = "",
-         y = "") +
+         x = "Longitude",
+         y = "Latitude") +
     theme(plot.caption = element_text(hjust = 0.5)) +
     ggtitle("Distribution of total population of immates across America")
 }
